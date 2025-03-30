dbfcb467ac249041d845dd525e8f800cf818cc3a
emfo2405 <emfo2405@student.miun.se>
2025-03-30 15:31:45 +0200

    Funktion som skriver ut nya element i listan på sidan

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,28 @@ function printCourseInfo (course: courseInfo) : void {
   }
 }
 
+let courseForm = document.getElementById("courseForm") as HTMLFormElement;
+
+courseForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let courseCode = document.getElementById("code") as HTMLInputElement;
+    let courseName = document.getElementById("name") as HTMLInputElement;
+    let courseProgression = document.getElementById("progression") as HTMLInputElement;
+    let courseSyllabus = document.getElementById("url") as HTMLSelectElement;
+
+    let newCourse: courseInfo = {
+        code: courseCode.value,
+        name: courseName.value,
+        progression: courseProgression.value,
+        syllabus: courseSyllabus.value
+
+    };
+    
+    printCourseInfo(newCourse);
+
+    courseForm.reset();
+});
+
+
 
