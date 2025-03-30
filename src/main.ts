1d08b9cfee42e319c9e4bc794cf3edec05bc53b4
emfo2405 <emfo2405@student.miun.se>
2025-03-30 15:30:48 +0200

    funktion för att lägga till element i lista skapad

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,18 @@ interface courseInfo {
   syllabus: string;
 }
 
+function printCourseInfo (course: courseInfo) : void {
+  let courseList = document.getElementById("course-list");
+
+  if (courseList) {
+      courseList.innerHTML += `
+      <li>
+      ${course.code}, 
+      ${course.name}, 
+      ${course.progression}, 
+      <a href="${course.syllabus}">Kursplan</a></li>
+      `;
+  }
+}
+
+
